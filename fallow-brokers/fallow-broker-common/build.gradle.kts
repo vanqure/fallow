23b89df120960dd65757494be50cdc563b4522f4
vanqure <sergiuszmecina@icloud.com>
2025-08-25 18:21:05 +0200

    removed unit tests since they are already in readme.md

--- fallow-brokers/fallow-broker-common/build.gradle.kts
+++ fallow-brokers/fallow-broker-common/build.gradle.kts
@@ -7,8 +7,6 @@ plugins {
 dependencies {
     api(project(":fallow-codecs:fallow-codec-common"))
     api(libs.wisp)
-    testImplementation(project(":fallow-codecs:fallow-codec-jackson"))
-    testImplementation(project(":fallow-brokers:fallow-broker-redis"))
 }
 
 fallowPublish {
